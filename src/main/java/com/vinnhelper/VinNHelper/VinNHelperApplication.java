63976a0cd175e7d04c3d96eac5cd802eb8329cf6
BaldaAzz <kill200419@gmail.com>
2024-09-29 19:03:53 +0300

    create project

---
+++ src/main/java/com/vinnhelper/VinNHelper/VinNHelperApplication.java
@@ -0,0 +1,13 @@
+package com.vinnhelper.VinNHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinNHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinNHelperApplication.class, args);
+	}
+
+}
